87783206e2b3090536fdce13008c7ad0337c8554
Farid Hammane <fhammane@gmail.com>
2018-06-23 12:45:44 +0200

    Introduce add_library & split into 2 projects: main and fact
    Create a new project for the library 'fact' and include it
    with add_subdirectory. Its properties are added to the main
    project.

---
+++ lib/inc/fact.h
@@ -0,0 +1,8 @@
+#if ! defined (FACT_H)
+#define FACT 1
+
+unsigned long long fact (int val);
+
+#endif
+
+
--- main/main.c
+++ main/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned long long fact (int val);
+#include <fact.h>
 
 int main (int argc, char **argv)
 {
