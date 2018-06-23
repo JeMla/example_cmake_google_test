9315a691df0ba32f7fcea530335a2f6adbc147ca
Farid Hammane <fhammane@gmail.com>
2018-06-23 14:39:45 +0200

    Add a "Configured Header" File
    Inspired from https://cmake.org/cmake-tutorial/

--- main/main.c
+++ main/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <factConfig.h>
 #include <fact.h>
 
 int main (int argc, char **argv)
 {
 	int value;
 	unsigned long long computed;
+
+    fprintf(stdout, "PROJECT '%s'\n", PROJECT);
+    fprintf(stdout, "PROJECT_VERSION '%s'\n", PROJECT_VERSION);
+    fprintf(stdout, "PROJECT_VERSION_MAJOR '%s'\n", PROJECT_VERSION_MAJOR);
+    fprintf(stdout, "PROJECT_VERSION_MINOR '%s'\n", PROJECT_VERSION_MINOR);
+    fprintf(stdout, "PROJECT_VERSION_PATCH '%s'\n", PROJECT_VERSION_PATCH);
+    fprintf(stdout, "PROJECT_VERSION_TWEAK '%s'\n", PROJECT_VERSION_TWEAK);
 
 	if (argc != 2) {
 		fprintf(stderr, "Missing arguments\n");
